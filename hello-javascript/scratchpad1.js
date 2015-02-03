c35b536695463ef8c91aacefdbf0f8d0098a5d15
melekkahraman <melekkahraman>
2015-02-03 12:02:36 +0200

    hello-javascript

---
+++ hello-javascript/scratchpad1.js
@@ -0,0 +1,40 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var x = 5;
+console.log(x);
+x * 5 + 3;
+y; 
+5 / 0; /* Infinity */
+
+x = "cat";   // dynamic typing x int değeri tutarken string tutmaya başladı
+var arr = [1, 2, 3, 4, "cat"]; // dynamic typing
+y = "5";
+y == 5; 
+
+y === 5; // false, checks the types also 
+
+z = "0";
+if (z) {
+  console.log("Success");
+}
+else {
+  console.log("Fail");
+}
+
+var zombi = null;
+if (zombi == null) {
+  console.log("Success");
+}
+else {
+  console.log("Fail");
+}
+
+
+
